62d18a412adc3f30199199a5c0f4aa590c843c7c
jdolivo <martindolivo@gmail.com>
2023-06-13 13:10:50 -0300

    csv reader bien

--- OBLIGATORIO 2023/src/Main.java
+++ OBLIGATORIO 2023/src/Main.java
@@ -45,7 +45,9 @@ public class Main {
 
                 case 7:
                     System.out.println("Salir del programa");
+                    sc.close();
                     break;
+
 
                 default:
                     System.out.println("Opcion no valida,vuelva a intentarlo");
@@ -88,6 +90,7 @@ public class Main {
     }
     public static void opcion3(Scanner sc){
         System.out.println("Digite el dia en notacion YYYY-MM-DD: ");
+        sc.nextLine();
         String format = sc.nextLine();
         LocalDate dia = LocalDate.parse(format);
         CargaDeDatos.hashtagsDistintosDia(dia);
@@ -96,6 +99,7 @@ public class Main {
     public static void opcion4(Scanner sc){
 
         System.out.println("Digite el dia en notacion YYYY-MM-DD: ");
+        sc.nextLine();
         String format = sc.nextLine();
         LocalDate dia = LocalDate.parse(format);
     }
