73769a68e33a25ba5a6a4e81783fd2d84038fdef
esugasti <esugasti@correo.um.edu.uy>
2023-06-23 23:44:47 -0300

    se agregaron hashtables para que las consultas sean mas rapidas

--- OBLIGATORIO 2023/src/CargaDeDatos.java
+++ OBLIGATORIO 2023/src/CargaDeDatos.java
@@ -3,6 +3,7 @@ import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import uy.edu.um.adt.Hash.HashImpl;
 import uy.edu.um.adt.Hash.MyHash;
+
 import uy.edu.um.adt.LinkedList.MyLinkedListImpl;
 import uy.edu.um.adt.LinkedList.MyList;
 import uy.edu.um.adt.Queue.EmptyQueueException;
@@ -18,24 +19,27 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+
 import java.util.Objects;
 
 public class CargaDeDatos {
-    public static MyList<Tweet> tweets;
+    public static MyHash <Integer, Tweet> todoslosTweets;
     public static MyList<Pilotos> pilotos;
     public static MyHash<Long,Hashtag> hashtagsdiferentes;
     public static MyList<Hashtag> hashtagsdiferentesLinkedList;
     public static MyHash<Long,User> usuarios;
-    public static MyList<User> usuariosLinkedList;
+    public static MyHash<Integer,User> listaUsuarios;
     public static int cantidadTweets;
     public static int cantidadUsuarios;
     public static int cantidadPilotos;
     public static int cantidadHashtagsDif;
+
+
     public static void cargaDeDatos() throws IOException, NumberFormatException {
         Reader in = new FileReader("OBLIGATORIO 2023/Data/f1_dataset_test.csv");
-        tweets = new MyLinkedListImpl<Tweet>();
+        todoslosTweets = new HashImpl<>(200000);
         usuarios= new HashImpl<>(30000);
-        usuariosLinkedList=new MyLinkedListImpl<User>();
+        listaUsuarios=new HashImpl<>(30000);
         hashtagsdiferentes= new HashImpl<>(250000);
         hashtagsdiferentesLinkedList=new MyLinkedListImpl<Hashtag>();
         CSVParser parser = new CSVParser(in, CSVFormat.DEFAULT);
@@ -86,16 +90,17 @@ public class CargaDeDatos {
             }
             long idtweet=generateUniqueId(paraFecha);
             Tweet tweet = new Tweet(idtweet,texto, username, fechatweet, source, favoritos, isretweet);
+            User user = new User(idUser, username, verified);
 
+            todoslosTweets.put(cantidadTweets, tweet); // agregamos el tweet a la hashtable de todos los tweets
+            listaUsuarios.put(cantidadUsuarios, user);
 
 
             cantidadTweets++;
-            tweets.add(tweet);
             if (!usuarios.contains(idUser)){  //Si no esta el usuario, lo agrega
                 User usuarioTemp = new User(idUser,username,verified);
                 usuarioTemp.addTweet(tweet.getId(),tweet);
                 usuarios.put(usuarioTemp.getId(),usuarioTemp);
-                usuariosLinkedList.add(usuarioTemp);
                 cantidadUsuarios++;
                  //LOS USUARIOS SE AGREGAN BIEN
                 }
@@ -120,12 +125,11 @@ public class CargaDeDatos {
         }
 
 
-
         parser.close();
         in.close();
 
     }
-    public static void nombrePilotos(){  //Para leer los pilotos, funciona
+    public static void nombrePilotos(){  //Para leer los pilotos
         String busquedaPilotos="OBLIGATORIO 2023/Data/drivers.txt";
         pilotos=new MyLinkedListImpl<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(busquedaPilotos))) {
@@ -140,11 +144,14 @@ public class CargaDeDatos {
         }
     }
 
+
+
+
     //Consulta 1
     public static void pilotosMasMencionados(LocalDate fecha) throws EmptyQueueException {
         ImplPriorityQueue<Pilotos> pilotosMasMencionados = new ImplPriorityQueue<>();
         for (int i=0;i<cantidadTweets;i++){
-            Tweet tweettemp=tweets.get(i);
+            Tweet tweettemp=todoslosTweets.get(i);
             int anio1=fecha.getYear();
             int anio2=tweettemp.getDate().getYear();
             boolean iguales=anio1 == anio2;
@@ -185,7 +192,7 @@ public class CargaDeDatos {
     public static void usuariosConMasTweets() throws EmptyQueueException{
         ImplPriorityQueue<User> usuariosMasTweets = new ImplPriorityQueue<>();
         for (int i = 0; i <cantidadUsuarios ; i++) {
-            User usuarioT = usuariosLinkedList.get(i);
+            User usuarioT = listaUsuarios.get(i);
             int prioridad=usuarioT.getCantidadTweets();
             usuariosMasTweets.enqueueWithPriority(usuarioT,prioridad);
 
@@ -210,10 +217,10 @@ public class CargaDeDatos {
     //Consulta 3
     public static int hashtagsDistintosDia(LocalDate dia){
 
-        int cantidad=0;     //ACA FALTA PERO ES POR ACA
+        int cantidad=0;
         MyHash hashtagsdia=new HashImpl<Long,Hashtag>(500);
         for (int i = 0; i < cantidadTweets; i++) {
-            Tweet tweettemp=tweets.get(i);
+            Tweet tweettemp=todoslosTweets.get(i);
             if(dia.isEqual(tweettemp.getDate().toLocalDate())){
                 for (int j = 0; j < tweettemp.getCantidadHashtags(); j++) {
                     Hashtag tempId= (Hashtag) tweettemp.getHashtags().get(j);
@@ -231,8 +238,8 @@ public class CargaDeDatos {
     public static void hashtagMasUsadoDia(LocalDate dia) throws EmptyQueueException {
         ImplPriorityQueue<Hashtag> hashtagsMasUsados = new ImplPriorityQueue<>();
         for (int i = 0; i < cantidadTweets; i++) {
-            if (dia.compareTo(tweets.get(i).getDate().toLocalDate()) == 0) {
-                Tweet tweettemp = tweets.get(i);
+            if (dia.compareTo(todoslosTweets.get(i).getDate().toLocalDate()) == 0) {
+                Tweet tweettemp = todoslosTweets .get(i);
                 int cantidad = tweettemp.getCantidadHashtags();
                 for (int j = 0; j < cantidad; j++) {
                     Hashtag hashtagtemp = (Hashtag) tweettemp.getHashtags().get(j);
@@ -254,7 +261,7 @@ public class CargaDeDatos {
     public static void usuariosMasLikeados() throws EmptyQueueException {
         ImplPriorityQueue<User> usuariosMasLikeados=new ImplPriorityQueue<>();
         for (int i = 0; i <cantidadUsuarios ; i++) {
-            User usuarioT = usuariosLinkedList.get(i);
+            User usuarioT = listaUsuarios.get(i);
             int prioridad=usuarioT.getFavoritos();
             usuariosMasLikeados.enqueueWithPriority(usuarioT,prioridad);
         }
@@ -275,7 +282,7 @@ public class CargaDeDatos {
         int cantidad = 0;
         String content = "";
         for (int i = 0; i < cantidadTweets; i++) {
-            content = tweets.get(i).getContent();
+            content = todoslosTweets.get(i).getContent();
             if (content.contains(frase)) {
                 cantidad++;
             }
